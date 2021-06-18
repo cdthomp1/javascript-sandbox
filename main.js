3c820cd82d79a365033ee72ac6eeaaf0619dcf92
cdthomp1 <camthomp96@gmail.com>
2021-06-18 17:53:44 -0600

    init

---
+++ main.js
@@ -0,0 +1,47 @@
+import Rocket from './rocket.js'
+
+document.addEventListener('keydown', logKey);
+
+var canvas = document.getElementById('tutorial');
+var ctx = canvas.getContext('2d');
+var raf;
+
+
+var rocket = new Rocket(0, canvas.height - 100, canvas);
+
+function draw(vy) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    rocket.draw();
+    if (rocket.y + rocket.vy > canvas.height || rocket.y + rocket.vy < 0) {
+        rocket.vy = -rocket.vy;
+    }
+    if (rocket.x + rocket.vx > canvas.width || rocket.x + rocket.vx < 0) {
+        rocket.vx = -rocket.vx;
+    }
+    rocket.y += rocket.vy;
+    raf = window.requestAnimationFrame(draw);
+}
+
+canvas.addEventListener('mouseover', function (e) {
+    raf = window.requestAnimationFrame(draw);
+});
+
+canvas.addEventListener('mouseout', function (e) {
+    window.cancelAnimationFrame(raf);
+});
+
+function logKey(e) {
+    if (e.keyCode === 87) { // w
+        rocket.draw()
+    } else if (e.keyCode === 65) { // a
+        rocket.rollLeft(10)
+        console.log(rocket.data)
+    } else if (e.keyCode === 83) { // s
+        rocket.minusThrust(10)
+        console.log(rocket.data)
+    } else if (e.keyCode === 68) { // d
+        rocket.rollRight(10)
+        console.log(rocket.data)
+    }
+}
+
---
+++ rocket.js
@@ -0,0 +1,58 @@
+export default class Rocket {
+    constructor(x, y, canvas) {
+        this.velocity = 0;
+        this.roll = 0;
+        this.x = x;
+        this.y = y;
+        this.vx = 0
+        this.vy = 5;
+        this.radius = 25;
+        this.canvas = canvas;
+        this.draw();
+        this.image = new Image();
+        this.src = './rocket.png';
+        this.image.src = this.src;
+    }
+
+    get data() {
+        return { x: this.x, y: this.y, vx: this.vx, vy: this.vy };
+    }
+
+    addThrust(thrust) {
+        if (this.velocity !== 100) {
+            this.y -= this.vy;
+        } else {
+            console.log('Too Fast')
+        }
+    }
+
+    minusThrust(thrust) {
+        this.velocity -= thrust;
+    }
+
+    rollLeft(roll) {
+        this.roll -= roll;
+    }
+
+    rollRight(roll) {
+        this.roll += roll;
+    }
+
+    draw() {
+        var x = this.x;
+        var y = this.y;
+        var ctx = this.canvas.getContext('2d');
+        var img = new Image();
+        img.src = './rocket.png';
+        img.onload = function () {
+            ctx.drawImage(img, x, y, 100, 100);
+        };
+        /* var ctx = this.canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fill(); */
+    }
+}
+
